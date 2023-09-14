7cd4248e8a6fdf2dd90ffc55deee31cdcd9ec306
swimmingRiver <swimmingr@gmail.com>
2023-09-14 12:28:14 +0900

    코드 정리

--- src/App.js
+++ src/App.js
@@ -11,65 +11,66 @@ import ItemList from './Components/ItemList';
 import Card from './Components/Card';
 
 function App() {
-const [item,setItem]=useState('');
-const [budget,setBudget]=useState(0);
-const [editMode,setEditMode]=useState(false);
-const [total,setTotal]=useState();
-const [selectedId,setSelectedId] =useState(0);
-const [messageOn,setMessageOn]=useState('');
+  const [item, setItem] = useState('');
+  const [budget, setBudget] = useState(0);
+  const [editMode, setEditMode] = useState(false);
+  const [total, setTotal] = useState();
+  const [selectedId, setSelectedId] = useState(0);
+  const [messageOn, setMessageOn] = useState('');
 
-const list = useSelector((state)=>state.item);
-const dispatch = useDispatch();
+  const list = useSelector((state) => state.item);
+  const dispatch = useDispatch();
 
-useEffect(()=>{
-  setTotal(0);
-  list.map((el)=>setTotal((prev)=>prev+=el.budget));
-},[list]);
-const Submit=(e)=>{
-  e.preventDefault();
-  if(editMode){
-   dispatch(itemsReducer.actions.EDIT_ITEMS({
-      id:selectedId,
-      item:item,
-      budget:+budget,
-    }));
-    setMessageOn("수정");
+  useEffect(() => {
+    setTotal(0);
+    list.map((el) => setTotal((prev) => prev += el.budget));
+  }, [list]);
+
+  const Submit = (e) => {
+    e.preventDefault();
+    if (editMode) {
+      dispatch(itemsReducer.actions.EDIT_ITEMS({
+        id: selectedId,
+        item: item,
+        budget: +budget,
+      }));
+      setMessageOn("수정");
+    }
+    else {
+      dispatch(itemsReducer.actions.ADD_ITEMS({
+        id: list.length > 0 ? list[list.length - 1].id + 1 : 0,
+        item,
+        budget: +budget
+      }))
+      setMessageOn("생성");
+    }
+    setItem('');
+    setBudget(0);
+    setEditMode(false)
   }
-  else{
-  dispatch(itemsReducer.actions.ADD_ITEMS({
-    id:list.length>0?list[list.length-1].id+1:0,
-    item,
-    budget:+budget
-  }))
-  setMessageOn("생성");
-}
-  setItem('');
-  setBudget(0);
-  setEditMode(false)
-}
 
-const Reset=()=>{
-  dispatch(itemsReducer.actions.RESET_ITEMS());
-}
+  const Reset = () => {
+    dispatch(itemsReducer.actions.RESET_ITEMS());
+  }
 
 
 
 
 
-  
-useEffect(()=>{
-  setTimeout(()=>{setMessageOn("")},2000)
-},[messageOn]);
+
+  useEffect(() => {
+    setTimeout(() => { setMessageOn("") }, 2000)
+  }, [messageOn]);
 
   return (
     <div className="App">
       <header>
-        {messageOn===""?null:<MessageBox messageOn={messageOn}/>}
+        {messageOn === "" ? null : <MessageBox messageOn={messageOn} />}
         <h1>예산 계산기</h1>
       </header>
 
       <div className='Wrapper'>
-      <Form item={item}
+        <Form item={item}
           setItem={setItem}
           budget={budget}
           setBudget={setBudget}
@@ -79,22 +80,25 @@ useEffect(()=>{
           setSelectedId={setSelectedId}
           setMessageOn={setMessageOn}
           Submit={Submit}
-          Reset={Reset}/>
-      
-     <ItemList list={list} 
-     setItem={setItem}
-     setBudget={setBudget}
-     setEditMode={setEditMode}
-     setSelectedId={setSelectedId}
-     setMessageOn={setMessageOn}
-     itemsReducer={itemsReducer}
-     Card={Card}
-     />
-      <div>
-      <button id='resetBtn' onClick={Reset}><p>목록 지우기</p><span id='resetIcon' class="material-symbols-outlined">delete</span></button>
+          Reset={Reset} />
+
+        <ItemList list={list}
+          setItem={setItem}
+          setBudget={setBudget}
+          setEditMode={setEditMode}
+          setSelectedId={setSelectedId}
+          setMessageOn={setMessageOn}
+          itemsReducer={itemsReducer}
+          Card={Card}
+        />
+        <div>
+          <button id='resetBtn' onClick={Reset}>
+            <p>목록 지우기</p>
+            <span id='resetIcon' class="material-symbols-outlined">delete</span>
+            </button>
+        </div>
       </div>
-       </div>
-   <h1 id='total'>총 지출:{total}원</h1>
+      <h1 id='total'>총 지출:{total}원</h1>
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-const Form=({
+const Form = ({
     item,
     setItem,
     budget,
@@ -10,26 +10,26 @@ const Form=({
     setMessageOn,
     Submit,
     Reset,
-  })=>{
-    return(
-<form onSubmit={Submit}>
-       <div id="item">
-        <div>
-        <h2>지출항목</h2>
-        <input required value={item} type='text' placeholder='예)렌트비' onChange={(e)=>{setItem(e.target.value)}}/>
-        </div>
+}) => {
+    return (
+        <form onSubmit={Submit}>
+            <div id="item">
+                <div>
+                    <h2>지출항목</h2>
+                    <input required value={item} type='text' placeholder='예)렌트비' onChange={(e) => { setItem(e.target.value) }} />
+                </div>
 
-       <div>
-       <h2>비용</h2>
-        <input value={budget} type='number' onChange={(e)=>{setBudget(e.target.value)}}/>
-       </div>
-       </div>
-      
-     
-      <button id='submit'>
-      {editMode?<p>수정</p>:<p>제출</p>}
-      <span id='send' class="material-symbols-outlined">send</span>
-      </button>
-      </form>)
+                <div>
+                    <h2>비용</h2>
+                    <input value={budget} type='number' onChange={(e) => { setBudget(e.target.value) }} />
+                </div>
+            </div>
+
+
+            <button id='submit'>
+                {editMode ? <p>수정</p> : <p>제출</p>}
+                <span id='send' class="material-symbols-outlined">send</span>
+            </button>
+        </form>)
 }
 export default Form;
